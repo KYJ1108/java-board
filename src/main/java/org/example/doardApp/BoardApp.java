13085d9062c4c0f135e0ff4db9b1088595e4975c
unknown <kimyeji546408@gmail.com>
2024-03-13 13:46:45 +0900

    java correct testdate

--- src/main/java/org/example/doardApp/BoardApp.java
+++ src/main/java/org/example/doardApp/BoardApp.java
@@ -14,7 +14,17 @@ public class BoardApp {
 
         // 변수에는 하나의 값만 저장 가능하므로 여러개의 게시물을 저장하려면 ArrayList를 사용한다.
 
-        int latestArticleId = 1; // 게시물 번호 // 시작 번호를 1로 지정
+        // 시작 번호를 1로 지정
+        // 테스트 데이터 3개 있으므로 시작 번호 4로 지정
+        int latestArticleId = 4; // 게시물 번호
+
+        // 테스트 데이터
+        Article a1 = new Article(1, "안녕하세요 반갑습니다. 자바 공부중이에요.", "냉무", getCurrentDateTime());
+        Article a2 = new Article(2, "자바 질문좀 할게요~", "냉무", getCurrentDateTime());
+        Article a3 = new Article(3, "정처기 따야되나요?", "냉무", getCurrentDateTime());
+        articleList.add(a1);
+        articleList.add(a2);
+        articleList.add(a3);
 
         // 반복 횟수 정할 수 없음 => 무한 반복 구조
 
@@ -32,16 +42,8 @@ public class BoardApp {
                 System.out.print("게시물 내용을 입력해 주세요 : ");
                 String content = scan.nextLine();
 
-                LocalDateTime now = LocalDateTime.now();
-
-                // 날짜와 시간의 형식을 지정합니다. 여기서는 연-월-일 시:분:초 형식을 사용합니다.
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
-
-                // 지정한 형식으로 날짜와 시간을 출력합니다.
-                String formattedDate = now.format(formatter);
-
                 // 모든 매개변수를 받는 생성자 이용
-                Article article = new Article(latestArticleId, title, content, formattedDate);
+                Article article = new Article(latestArticleId, title, content, getCurrentDateTime());
 
                 articleList.add(article);
                 System.out.println("게시물이 등록되었습니다.");
@@ -125,5 +127,17 @@ public class BoardApp {
             }
         }
         return -1;
+    }
+
+    public String getCurrentDateTime(){
+        LocalDateTime now = LocalDateTime.now();
+
+        // 날짜와 시간의 형식을 지정합니다. 여기서는 연-월-일 시:분:초 형식을 사용합니다.
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+
+        // 지정한 형식으로 날짜와 시간을 출력합니다.
+        String formattedDate = now.format(formatter);
+
+        return formattedDate;
     }
 }
